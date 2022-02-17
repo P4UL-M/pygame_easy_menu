b65d559639c5bbe6e814869e39163217f82e19ad
P4UL-M <paul.mairesse@free.fr>
2022-02-17 15:08:47 +0100

    0.0.16

--- Setup.py
+++ Setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 setuptools.setup( 
     name="pygame_easy_menu", 
 
-    version="0.0.15", 
+    version="0.0.16", 
   
     author="Paul Mairesse",
     author_email="paul.mairesse@free.fr", 
