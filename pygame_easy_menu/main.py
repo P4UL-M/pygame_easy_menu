b202bcb8e5430253a5bf0987869cbc96ffac69ef
P4UL-M <paul.mairesse@free.fr>
2022-03-02 23:10:12 +0100

    small fix

--- pygame_easy_menu/main.py
+++ pygame_easy_menu/main.py
@@ -99,14 +99,13 @@ class Menu_Manager(object):
             py.display.update()
 
     def Update(self):
-        while self.running:
-            if self.actual_menu:
-                if self.actual_menu.background == None:
-                    self.screen.blit(self.background,(0,0))
-                else:
-                    self.screen.blit(self.actual_menu.background,(0,0))
-                self.actual_menu.Update()
-
+        if self.actual_menu:
+            if self.actual_menu.background == None:
+                self.screen.blit(self.background,(0,0))
+            else:
+                self.screen.blit(self.actual_menu.background,(0,0))
+            self.actual_menu.Update()
+            
     def stop(self):
         """
         stop the current thread de la classe
