cd2c0439a61f17c1ecbaada107b5c1cb752f73d9
P4UL-M <paul.mairesse@free.fr>
2022-03-02 22:36:27 +0100

    local update

--- Setup.py
+++ Setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 setuptools.setup( 
     name="pygame_easy_menu", 
 
-    version="0.0.19", 
+    version="0.0.20", 
   
     author="Paul Mairesse",
     author_email="paul.mairesse@free.fr", 
--- pygame_easy_menu/main.py
+++ pygame_easy_menu/main.py
@@ -98,6 +98,15 @@ class Menu_Manager(object):
                 self.actual_menu.Update()
             py.display.update()
 
+    def Update(self):
+        while self.running:
+            if self.actual_menu:
+                if self.actual_menu.background == None:
+                    self.screen.blit(self.background,(0,0))
+                else:
+                    self.screen.blit(self.actual_menu.background,(0,0))
+                self.actual_menu.Update()
+
     def stop(self):
         """
         stop the current thread de la classe
